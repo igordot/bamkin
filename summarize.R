12aedfea1a5cb8dc35aed7e2f1618230106c9e85
igor <6363505+igordot@users.noreply.github.com>
2018-06-19 12:59:07 -0400

    routine sync

--- summarize.R
+++ summarize.R
@@ -9,11 +9,11 @@
 
 
 # increase output width
-options(width = 150)
+options(width = 120)
 
 # command line arguments
 args = commandArgs(trailingOnly = TRUE)
-if(length(args) < 1) stop("usage: Rscript --vanilla summarize.R snp.freq.csv")
+if (length(args) < 1) stop("usage: Rscript --vanilla summarize.R <snp.freq.csv>")
 freq_file = args[1]
 
 # check that input files exist
@@ -29,24 +29,57 @@ for (p in package_list) {
   library(p, character.only = TRUE, quietly = TRUE)
 }
 
+# limits
+min_snps = 10
+max_snps = 10000
+
 message("importing ", freq_file)
 freq_table = read.csv(freq_file, row.names = 1, check.names = FALSE, stringsAsFactors = FALSE)
 freq_table = as.matrix(freq_table)
-message("cols/samples: ", ncol(freq_table))
-message("rows/snps input: ", nrow(freq_table))
 
-# filter for informative SNPs (varying frequencies across samples)
-freq_table = freq_table[rowSds(freq_table) > 0.1,]
-message("rows/snps variable: ", nrow(freq_table))
+# stop if too few SNPs in input file
+if (nrow(freq_table) < min_snps) stop("too few SNPs across all samples: ", nrow(freq_table))
 
-# subset to most variable SNPs to keep table size reasonable
-max_snps = 10000
+num_input_samples = ncol(freq_table)
+message("cols/samples: ", num_input_samples)
+num_input_snps = nrow(freq_table)
+message("rows/SNPs input: ", num_input_snps)
+
+# stop if too few variable SNPs
+num_var_snps = count(rowSds(freq_table) > 0.1)
+if (num_var_snps < min_snps) stop("too few variable SNPs: ", num_var_snps)
+
+# keep only informative SNPs with varying frequencies across samples
+freq_table = freq_table[rowSds(freq_table) > 0.1, ]
+
+# stop if too few highly variable (present at both high and low frequencies) SNPs
+num_high_var_snps = count((rowMaxs(freq_table) - rowMins(freq_table)) > 0.7)
+if (num_high_var_snps < min_snps) stop("too few ref/alt SNPs:", num_high_var_snps)
+
+message("rows/SNPs variable: ", nrow(freq_table))
+
+# subset to just most variable SNPs if table is too large
 if (nrow(freq_table) > max_snps) {
-  var_snps = order(rowVars(freq_table), decreasing = TRUE)[1:max_snps]
-  freq_table = freq_table[var_snps,]
+  message("subsetting table to most variable SNPs")
+  var_order_snps = order(rowVars(freq_table), decreasing = TRUE)[1:max_snps]
+  freq_table = freq_table[var_order_snps, ]
 }
 
-message("correlation")
+# save filtered table
+write.csv(cbind("#POS" = rownames(freq_table), freq_table), file = "snp.freq.filtered.csv", row.names = FALSE, quote = FALSE)
+
+# set ggplot geom_text font size based on number of samples (measured in mm, not points)
+if (num_input_samples > 50) {
+  font_size = 1.5
+} else if (num_input_samples > 20) {
+  font_size = 2
+} else if (num_input_samples > 10) {
+  font_size = 3
+} else {
+  font_size = 4
+}
+
+# correlation
 freq_cor = cor(freq_table, method = "pearson")
 
 # correlation plot (clustered using hclust)
@@ -59,19 +92,31 @@ dev.off()
 # hiearchical cluster analysis using the euclidan distance between variables based on the correlation
 message("hierarchical cluster plot")
 freq_hclust = hclust(dist(freq_cor))
-hclust_plot = ggdendrogram(freq_hclust, rotate = TRUE) +
-theme(axis.text.x = element_blank())
+freq_dendro = dendro_data(freq_hclust, type = "rectangle")
+hclust_plot =
+  ggplot() +
+  geom_segment(data = segment(freq_dendro), aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_text(data = label(freq_dendro), aes(x = x, y = y, label = label, hjust = 0), size = font_size) +
+  coord_flip() +
+  scale_y_reverse(expand = c(0.8, 0)) +
+  theme(axis.text = element_blank(), axis.ticks = element_blank(), text = element_blank(), line = element_blank())
 save_plot(filename = "plot.hclust.png", plot = hclust_plot, base_width = 3, base_height = 5, units = "in", dpi = 300)
 
 # PCA
 message("PCA")
 pca = prcomp(t(freq_table))
 pca_data = data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2])
-pca_plot = ggplot(data = pca_data, aes_string(x = "PC1", y = "PC2")) +
-geom_point(size = 2) +
-theme(axis.text = element_blank(), axis.ticks = element_blank()) +
-geom_text_repel(aes(label = rownames(pca_data)))
+pca_plot =
+  ggplot(data = pca_data, aes_string(x = "PC1", y = "PC2")) +
+  geom_point(size = 2) +
+  geom_text_repel(aes(label = rownames(pca_data)),
+                  size = font_size, point.padding = unit(0.5, "lines"), color = "black") +
+  theme(aspect.ratio = 1, axis.text = element_blank(), axis.ticks = element_blank())
 save_plot(filename = "plot.pca.png", plot = pca_plot, base_width = 5, base_height = 5, units = "in", dpi = 300)
+
+# delete Rplots.pdf
+if (file.exists("Rplots.pdf")) file.remove("Rplots.pdf")
+
 
 
 # end
